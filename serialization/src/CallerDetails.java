d249bee503856d91bcf249b7a716a28f28ce8469
kishore T <kishore.tamilselvan@anywhere.co>
2023-05-25 22:06:54 +0530

    Initial update

--- serialization/src/CallerDetails.java
+++ serialization/src/CallerDetails.java
@@ -6,10 +6,6 @@ public class CallerDetails implements Serializable {
     private String purpos="Business Purpose";
     @Override
     public String toString() {
-        return "CallerDetails{" +
-                "caller_id=" + caller_id +
-                ", caller_name='" + caller_name + '\'' +
-                ", purpos='" + purpos + '\'' +
-                '}';
+        return "CallerDetails{" + "caller_id=" + caller_id + ", caller_name='" + caller_name + '\'' + ", purpos='" + purpos + '\'' + '}';
     }
 }
